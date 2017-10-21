0366d4b85f143a8f2df737171134e84215c62e84
BenjaminBLi <li.benjamin.b@gmail.com>
2017-10-21 17:22:20 -0400

    Created base templates for user profile and leaderboards.

--- DietTracker/urls.py
+++ DietTracker/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^$', include('leaderboard.urls')),
+    url(r'^leaderboard/', include('leaderboard.urls')),
+    url(r'^myProfile/', include('myProfile.urls')),
     url(r'^admin/', admin.site.urls),
 ]
